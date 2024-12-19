e5f7e222f3e4024b777a96b4a5072f86814dc16d
unbyte <i@shangyes.net>
2024-12-19 08:55:16 +0000

    feat: write to output dir directly

--- src/cli/download-vscode.ts
+++ src/cli/download-vscode.ts
@@ -1,20 +1,18 @@
-import { rename, rm } from 'node:fs/promises'
+import { mkdir, rm } from 'node:fs/promises'
 import { Readable } from 'node:stream'
 import { pipeline } from 'node:stream/promises'
 import d from 'debug'
 import gunzip from 'gunzip-maybe'
 import { extract } from 'tar-fs'
 import type { NormalizedConfig } from './config'
-import { progress, tempdir } from './utils'
+import { progress } from './utils'
 
 const debug = d('codem:download-vscode')
 
 export async function downloadVscode(config: NormalizedConfig) {
-  await using downloadDir = await tempdir()
-  debug('download directory', downloadDir.path)
-
   debug('remove output directory', config.output)
   await rm(config.output, { recursive: true, force: true })
+  await mkdir(config.output, { recursive: true })
 
   const specifier = config.commit ? `commit:${config.commit}` : config.version
   debug('specifier', specifier)
@@ -41,11 +39,9 @@ export async function downloadVscode(config: NormalizedConfig) {
       raw,
       bar.stream,
       gunzip(),
-      extract(downloadDir.path, { strip: 1 }),
+      extract(config.output, { strip: 1 }),
     )
   } catch (error) {
     throw new Error('failed to extract VSCode', { cause: error })
   }
-
-  await rename(downloadDir.path, config.output)
 }
--- src/cli/utils.ts
+++ src/cli/utils.ts
@@ -1,17 +1,4 @@
-import { mkdtemp, rm } from 'node:fs/promises'
-import { tmpdir } from 'node:os'
-import { join } from 'node:path'
 import { Transform } from 'node:stream'
-
-export async function tempdir() {
-  const path = await mkdtemp(join(tmpdir(), 'codeweb-'))
-  return {
-    path,
-    async [Symbol.asyncDispose]() {
-      await rm(path, { recursive: true, force: true })
-    },
-  }
-}
 
 export function progress(label: string, total: number) {
   let current = 0
